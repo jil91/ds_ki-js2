0c1c9e73958586019e8180bc1b18121add3c1632
jil91 <jil91@users.noreply.github.com>
2025-09-26 04:03:04 +0000

    deploy: eb4ff762883aa73505319705c950bc6ae1ee815f

--- _sources/oop/t01_einstieg.md
+++ _sources/oop/t01_einstieg.md
@@ -3,9 +3,15 @@
 <iframe src="../_static/pdfs/t01_einstieg.pdf" width="800" height="600" style="border: none;"></iframe>
 </p>
 
-<p>
-Hier klicken, um die die Datei als PDF anzusehen:
-
-<a href="../_static/pdfs/t01_einstieg.pdf" target="_blank" rel="noopener noreferrer">Einstieg OOP (PDF)</a>
-
-</p>
+<div style="border: 2px solid #007ACC; padding: 15px; border-radius: 8px; background-color: #F0F8FF; margin: 20px 0;">
+<p style="text-align: center; margin: 0;">
+    Hier klicken, um die die Datei als PDF anzusehen:
+    <br>
+     <a href="../_static/pdfs/t01_einstieg.pdf" 
+           target="_blank" 
+           rel="noopener noreferrer" 
+           style="color: #007ACC; font-weight: bold; text-decoration: none;">
+            Einstieg OOP
+        </a>
+    </p>
+</div>
--- _static/tippy/oop/t01_einstieg.527bc741-1405-4784-b87b-e78b0da712ae.js
+++ _static/tippy/oop/t01_einstieg.527bc741-1405-4784-b87b-e78b0da712ae.js
@@ -1,4 +1,4 @@
-selector_to_html = {"a[href=\"#oop-einstieg\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\"><span class=\"section-number\">2. </span>OOP - Einstieg<a class=\"headerlink\" href=\"#oop-einstieg\" title=\"Link to this heading\">#</a></h1><p>\n<iframe height=\"600\" src=\"../_static/pdfs/t01_einstieg.pdf\" style=\"border: none;\" width=\"800\"></iframe>\n</p><p>\nHier klicken, um die die Datei als PDF anzusehen:\n<p><a href=\"../_static/pdfs/t01_einstieg.pdf\" rel=\"noopener noreferrer\" target=\"_blank\">Einstieg OOP (PDF)</a></p>\n</p>"}
+selector_to_html = {"a[href=\"#oop-einstieg\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\"><span class=\"section-number\">2. </span>OOP - Einstieg<a class=\"headerlink\" href=\"#oop-einstieg\" title=\"Link to this heading\">#</a></h1><p>\n<iframe height=\"600\" src=\"../_static/pdfs/t01_einstieg.pdf\" style=\"border: none;\" width=\"800\"></iframe>\n</p>"}
 skip_classes = ["headerlink", "sd-stretched-link"]
 
 window.onload = function () {
